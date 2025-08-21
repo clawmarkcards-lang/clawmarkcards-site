d314f7aff35e5d0b583835999764a4024b48cd16
clawmarkcards-lang <clawmarkcards@gmail.com>
2025-08-21 09:29:59 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase-config.js
 const firebaseConfig = {
-  apiKey: "5a6b5354ace19c62a2747f46e9c0c1fede188476",
-  authDomain: "clawmark-login.firebaseapp.com",
-  projectId: "clawmark-login",
-  storageBucket: "clawmark-login.firebasestorage.app",
-  messagingSenderId: "334338665183",
-  appId: "1:334338665183:web:eb179b820393234c74576a",
-  measurementId: "G-JE64ZQ56T5"
+  apiKey: "AIzaSyCM5La0C7dKpWgQUpAxVQguDcEV1D4Vmms",
+  authDomain: "clawmarkcards-app.firebaseapp.com",
+  projectId: "clawmarkcards-app",
+  storageBucket: "clawmarkcards-app.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
+// Initialize Firebase (for compat)
+firebase.initializeApp(firebaseConfig);
